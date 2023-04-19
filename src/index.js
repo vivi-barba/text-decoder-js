a7cc4bc7312d924d717bd507e4f96a11693017ed
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-04-19 11:06:41 -0300

    set style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("oi")
+//Variaveis pegando a entrada inserida para ser criptografada e descriptografada
+const textCriptografar = document.getElementById("textCriptografar");
+const buttonCriptografar = document.getElementById("buttonCriptografar");
+const buttonDescriptografar = document.getElementById("buttonDescriptografar");
+
+//Function 
+
+function convertCripto(text)
+
+console.log(textCriptografar)
